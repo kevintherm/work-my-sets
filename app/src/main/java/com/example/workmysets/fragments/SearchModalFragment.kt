a3cd48f8acf27d9a8d3ef67d004e470fb4160f78
Kevin <kevindarmawan022@gmail.com>
2025-08-10 12:31:36 +0700

    change launcher icon

--- app/src/main/java/com/example/workmysets/fragments/SearchModalFragment.kt
+++ app/src/main/java/com/example/workmysets/fragments/SearchModalFragment.kt
@@ -6,15 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.workmysets.R
+import com.example.workmysets.databinding.FragmentFilterBottomSheetBinding
+import com.example.workmysets.databinding.FragmentSearchModalBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class SearchModalFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentSearchModalBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search_modal, container, false)
+        _binding = FragmentSearchModalBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
